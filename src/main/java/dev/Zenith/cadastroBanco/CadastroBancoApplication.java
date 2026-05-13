29b5f7c49ee36bfca19dcd1f89ddf1230df0ac3a
RodrygoLimeira117 <rodrigolimeira2002@outlook>
2026-05-12 22:41:26 -0300

    first commit

---
+++ src/main/java/dev/Zenith/cadastroBanco/CadastroBancoApplication.java
@@ -0,0 +1,13 @@
+package dev.Zenith.cadastroBanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroBancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroBancoApplication.class, args);
+	}
+
+}
